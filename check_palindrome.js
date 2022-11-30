b79dd13bfa79d026d1fe6591fbd38f7263c4b9f7
abdulrashid06 <abdulrashid987655@gmail.com>
2022-11-30 16:17:37 +0530

    check the code once

---
+++ check_palindrome.js
@@ -0,0 +1,11 @@
+let str="madam";
+
+ let bag=""
+ for (let i=str.length-1; i>=0; i++){
+     bag = str[i];
+ }
+ if (str==bag){
+     console.log("Yes")
+ }else{
+     console.log("No")
+ }
---
+++ check_prime.js
@@ -0,0 +1,12 @@
+let num=13;
+for (let i=0; i<num; i++{
+   if (num%i==0){
+     factor++;
+   }
+ }
+ if (factor==2){
+   console.log("Yes");
+ }
+ else{
+   console.log("No");
+ }
