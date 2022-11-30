6ff7af399ffed88bae2c5827b69f28118caba220
abdulrashid06 <abdulrashid987655@gmail.com>
2022-11-30 22:06:20 +0530

    now work done

--- check_prime.js
+++ check_prime.js
@@ -1,11 +1,11 @@
 let num=6;
 let factor=0;
-for (let i=0; i<num; i++){
+for (let i=1; i<=num; i++){
    if (num%i==0){
      factor++;
    }
  }
- if (factor==1){
+ if (factor==2){
    console.log("Yes");
  }
  else{
